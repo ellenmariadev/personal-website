e54d4de2bc8998f5ea067bd592dcf90617081e78
ellenmariadev <ellenmariadev@gmail.com>
2022-06-25 23:09:27 -0300

    add main.js + change style

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//-------SKILLS BUTTON-------//
 const btnDev = document.querySelector('.btn-skills.dev')
 const btnDesign = document.querySelector('.btn-skills.design')
 const designContent = document.querySelector('.designContent')
@@ -18,8 +19,34 @@ btnDesign.addEventListener('click', () => {
 })
 
 
-const div = document.querySelector('.repositories')
+//-------NAVBAR-------//
+const options = {
+    threshold: 0.45
+}
+const observer = new IntersectionObserver(entries => {
 
+    entries.forEach((entry) => {
+        const id = entry.target.getAttribute('id')
+
+        if (entry.isIntersecting) {
+            document.querySelector(`nav li a[href="#${id}"]`).classList.add('nav-scrolled');
+        } else {
+            document.querySelector(`nav li a[href="#${id}"]`).classList.remove('nav-scrolled')
+        }
+    })
+}, options);
+
+const sections = document.querySelectorAll('main, section')
+sections.forEach((section) => {
+    observer.observe(section)
+})
+
+const navigationHeight = document.querySelector('header').offsetHeight;
+document.documentElement.style.setProperty('--scroll-padding', navigationHeight - 1 + 'px')
+
+
+//-------API GITHUB-------//
+const div = document.querySelector('.repositories')
 function getApiGitHub() {
     fetch('https://api.github.com/users/ellenmariadev/repos')
         .then(async res => {
@@ -43,16 +70,13 @@ function getApiGitHub() {
                     </li>
                 </ul>
                 <ul class="tags">
-                <li><a href="${item.homepage}" target="_blank" rel="noopener noreferrer">DEMO</a></li>
-                <li><a href="${item.html_url}" target="_blank" rel="noopener noreferrer">CODE</a></li>
+                <li><a href="${item.homepage}" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-link"></i> DEMO</a></li>
+                <li><a href="${item.html_url}" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github"></i> CODE</a></li>
                 </ul>
             </div>`
 
-
                 div.append(content)
-
             })
-
         }).catch(e => console.log(e))
 }
 
